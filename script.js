82cbec6738276ccb84840ee823b974c7363b1e27
Ismeny Saguilan <menysag2@gmail.com>
2021-10-01 19:39:01 -0500

    final commit

--- script.js
+++ script.js
@@ -23,18 +23,54 @@ var textAreaEl = $("textarea");
 var saveBtn = $(".saveBtn");
 saveBtn.on("click", userInput);
 
-function userInput(){
+//grabbing id values from html
+function userInput() {
     var input = $(this).siblings("textarea").val();
     console.log(input);
-    localStorage.setItem(textAreaEl.attr("id"),input)
-   return;
+    var idEl = $(this).siblings("textarea").attr("id");
+    localStorage.setItem(idEl, input);
+    return;
 }
 
 
 function storedInput() {
-    textAreaEl.textContent = localStorage.getItem("9am");
-    console.log("init")
-    console.log(textAreaEl)
-}
+    var time1 = localStorage.getItem("9am");
+    $("#9am").text(time1)
+    var time2 = localStorage.getItem("10am");
+    $("#10am").text(time2)
+    var time3 = localStorage.getItem("11am");
+    $("#11am").text(time3)
+    var time4 = localStorage.getItem("12pm");
+    $("#12pm").text(time4)
+    var time5 = localStorage.getItem("1pm");
+    $("#1pm").text(time5)
+    var time6 = localStorage.getItem("2pm");
+    $("#2pm").text(time6)
+    var time7 = localStorage.getItem("3pm");
+    $("#3pm").text(time7)
+    var time8 = localStorage.getItem("4pm");
+    $("#4pm").text(time8)
+    var time9 = localStorage.getItem("5pm");
+    $("#5pm").text(time9)
 
-  storedInput();
+}
+storedInput();
+
+function colorCode() {
+    moment().hour();
+    console.log(moment().hour());
+    $("textarea").each(function () {
+        console.log("okay");
+        $(this).attr("attr");
+        if (moment().hour() == $(this).attr("attr")) {
+            $("textarea").addClass("present");
+        }else if (moment().hour() < $(this).attr("attr")) {
+                $("textarea").addClass("future");
+        }else {
+            $("textarea").addClass("past");
+        }
+    });
+
+            
+}
+colorCode();
